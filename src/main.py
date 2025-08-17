b878b6dcbe68564e53c2b4a40b3a6ec9d5cad163
regusan-laptop-ubuntu <dammy@gmail.com>
2025-08-17 18:25:08 +0900

    [追加]ペルソナファイルを追加

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from Role import Role
 from Brain import Brain
 from GeminiBrain import GeminiBrain
 from UserBrain import UserBrain
+from OllamaBrain import OllamaBrain
+import Persona 
 import random
 
 def main():
@@ -14,10 +16,14 @@ def main():
     persona = ""
 
     agents = [
-        Agent("太郎", brain=UserBrain(), role= gs.roles[gs.人狼], persona=persona),
-        Agent("花子", brain=GeminiBrain(), role= gs.roles[gs.占い師], persona=persona),
-        Agent("一郎", brain=GeminiBrain(), role= gs.roles[gs.市民], persona=persona),
-        Agent("二郎", brain=GeminiBrain(), role= gs.roles[gs.市民], persona=persona),
+        Agent("一郎", brain=GeminiBrain(), role= gs.roles[gs.市民], persona=Persona.皮肉屋ペルソナ),
+        Agent("二郎", brain=GeminiBrain(), role= gs.roles[gs.市民], persona=Persona.調停者ペルソナ),
+        Agent("三郎", brain=GeminiBrain(), role= gs.roles[gs.市民], persona=Persona.分析家ペルソナ),
+        Agent("四郎", brain=GeminiBrain(), role= gs.roles[gs.市民], persona=Persona.癇癪ペルソナ),
+        Agent("じんた", brain=GeminiBrain(), role= gs.roles[gs.人狼], persona=Persona.分析家ペルソナ),
+        Agent("じん子", brain=GeminiBrain(), role= gs.roles[gs.人狼], persona=Persona.皮肉屋ペルソナ),
+        Agent("うらた", brain=GeminiBrain(), role= gs.roles[gs.占い師], persona=Persona.調停者ペルソナ),
+        Agent("れい子", brain=GeminiBrain(), role= gs.roles[gs.占い師], persona=Persona.分析家ペルソナ),
         ]
         
     allAgentName=", ".join([a.name for a in agents])
@@ -25,7 +31,9 @@ def main():
         agent.brain.UpdateSystemInstruction(gs.explainYours.format(agent=agent,allAgentName=allAgentName) + f"あなたの人格:\n{agent.persona}")
 
 
-    gmBrain = GeminiBrain(systemInstruction=gs.gmSystemInstruction.format(allAgentNames=allAgentName))
+    gmBrain = GeminiBrain(
+        #modelVirsion="gemini-2.5-pro",
+        systemInstruction=gs.gmSystemInstruction.format(allAgentNames=allAgentName))
 
     random.shuffle(agents)
     gm = GameMaster(agents=agents, gmBrain=gmBrain)
